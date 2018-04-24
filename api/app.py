283178db6678d637b389a6b8325fcd3782cc3aaa
Gevolll <tianxiao.wang@qq.com>
2018-04-24 21:18:38 +0800

    finish create course, solve CORS problem

--- api/app.py
+++ api/app.py
@@ -1,10 +1,33 @@
-from flask import flask
+from flask import Flask, Response
 from flask.ext.restful import Api, Resource
+from flask_cors import CORS
+from course import CreateCourse
+from werkzeug.datastructures import Headers
+
+# class MyResponse(Response):
+#     # Override Default Response Class
+#     def __init__(self, response=None, **kwargs):
+#         kwargs['headers'] = ''
+#         headers = kwargs.get('headers')
+#         # 跨域控制
+#         origin = ('Access-Control-Allow-Origin', '*')
+#         methods = ('Access-Control-Allow-Methods', 'HEAD, OPTIONS, GET, POST, DELETE, PUT')
+#         hs = ('Access-Control-Allow-Headers', 'content-type')
+#         if headers:
+#             headers.add(*origin)
+#             headers.add(*methods)
+#             headers.add(*hs)
+#         else:
+#             headers = Headers([origin, methods])
+#         kwargs['headers'] = headers
+#         return super().__init__(response, **kwargs)
 
 application = app = Flask(__name__)
+# app.response_class = MyResponse
 api = Api(app)
+CORS(app, origins='*', allow_headers='*')
 
-api.add_resource()
+api.add_resource(CreateCourse, '/createCourse', endpoint="createCourse")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- api/course.py
+++ api/course.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import Request
+from flask import request
 from flask.ext.restful import Resource
 from uuid import uuid4
 
@@ -11,28 +11,29 @@ tableTopicCourse = {}
 class CreateCourse(Resource):
     # 新建课程
     def post(self):
-        userId = request.form['userId']
-        courseTitle = request.form['courseTitle']
-        courseDescription = request.form['courseDescription']
-        topicCount = request.form['topics']['count']
-        topics = request.form['topics']['details']
+        data = request.get_json() or request.form
+        userId = data['userId']
+        courseTitle = data['courseTitle']
+        courseDescription = data['courseDescription']
+        topicCount = data['topics']['count']
+        topics = data['topics']['details']
 
-        courseId = uuid4()
-        tableCourseTeacher['courseId'] = userId
-        tableCourse['courseId'] = {
+        courseId = str(uuid4())
+        tableCourseTeacher[courseId] = userId
+        tableCourse[courseId] = {
             'courseTitle': courseTitle,
             'courseDescription': courseDescription
         }
         for i in range(0, topicCount):
-            topicId = uuid4()
-            tableTopicCourse['topicId'] = {
+            topicId = str(uuid4())
+            tableTopicCourse[topicId] = {
                 'courseId': courseId,
                 'topicTitle': topics[i]['topicTitle']
             }
 
-        return {
+        respData = {
             "courseId": courseId,
             "courseDetail": tableCourse[courseId]
-        }, 200, {
-            "Access-Control-Allow-Origin": "*"
         }
+
+        return respData
