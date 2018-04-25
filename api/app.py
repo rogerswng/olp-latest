1861692cc444b6b07ab7e9a84d8e6d3c90fa71d9
王天晓 <wangtianxiao@wangtianxiaos-MacBook-Pro.local>
2018-04-25 17:53:57 +0800

    add getteachercourselist

--- api/app.py
+++ api/app.py
@@ -1,26 +1,7 @@
 from flask import Flask, Response
 from flask.ext.restful import Api, Resource
 from flask_cors import CORS
-from course import CreateCourse
-from werkzeug.datastructures import Headers
-
-# class MyResponse(Response):
-#     # Override Default Response Class
-#     def __init__(self, response=None, **kwargs):
-#         kwargs['headers'] = ''
-#         headers = kwargs.get('headers')
-#         # 跨域控制
-#         origin = ('Access-Control-Allow-Origin', '*')
-#         methods = ('Access-Control-Allow-Methods', 'HEAD, OPTIONS, GET, POST, DELETE, PUT')
-#         hs = ('Access-Control-Allow-Headers', 'content-type')
-#         if headers:
-#             headers.add(*origin)
-#             headers.add(*methods)
-#             headers.add(*hs)
-#         else:
-#             headers = Headers([origin, methods])
-#         kwargs['headers'] = headers
-#         return super().__init__(response, **kwargs)
+from course import CreateCourse, TeacherCourseList
 
 application = app = Flask(__name__)
 # app.response_class = MyResponse
@@ -28,6 +9,7 @@ api = Api(app)
 CORS(app, origins='*', allow_headers='*')
 
 api.add_resource(CreateCourse, '/createCourse', endpoint="createCourse")
+api.add_resource(TeacherCourseList, '/teacherCourseList')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- api/course.py
+++ api/course.py
@@ -5,6 +5,7 @@ from flask.ext.restful import Resource
 from uuid import uuid4
 
 tableCourse = {}
+tableCourseStudent = {}
 tableCourseTeacher = {}
 tableTopicCourse = {}
 
@@ -19,21 +20,51 @@ class CreateCourse(Resource):
         topics = data['topics']['details']
 
         courseId = str(uuid4())
-        tableCourseTeacher[courseId] = userId
+        tableCourseTeacher[userId] = []
+        tableCourseTeacher[userId].append(courseId)
         tableCourse[courseId] = {
             'courseTitle': courseTitle,
             'courseDescription': courseDescription
         }
+        tableTopicCourse[courseId] = []
         for i in range(0, topicCount):
             topicId = str(uuid4())
+            tableTopicCourse[courseId].append(topicId)
             tableTopicCourse[topicId] = {
                 'courseId': courseId,
                 'topicTitle': topics[i]['topicTitle']
             }
-
         respData = {
             "courseId": courseId,
             "courseDetail": tableCourse[courseId]
+        }
+        return respData
+
+class TeacherCourseList(Resource):
+    # Fetch 课程列表
+    def get(self):
+        data = request.get_json() or request.form
+        userId = data['userId']
+        character = data['character']
+
+        respData = {}
+        courseList = tableCourseTeacher[userId]
+        courseDetail = []
+        courseCount = len(courseList)
+        for i in range(0, courseCount):
+            courseId = courseList[i]
+            topicIdList = tableTopicCourse[courseId]
+            topicList = map(lambda x: tableTopicCourse[x]['topicTitle'], topicIdList)
+            cd = {
+                "title": tableCourse[courseId]['courseTitle'],
+                "count": len(tableTopicCourse[courseId]),
+                "topicList": topicList
+            }
+            courseDetail.append(cd)
+
+        respData = {
+            "count": courseCount,
+            "courses": courseDetail
         }
 
         return respData
