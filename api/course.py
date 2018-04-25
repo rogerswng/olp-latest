4ba58daddbf88f253c423d77b0ab7f3433d312ec
王天晓 <wangtianxiao@wangtianxiaos-MacBook-Pro.local>
2018-04-25 18:09:01 +0800

    debug courseList

--- api/course.py
+++ api/course.py
@@ -44,27 +44,28 @@ class TeacherCourseList(Resource):
     # Fetch 课程列表
     def get(self):
         data = request.get_json() or request.form
-        userId = data['userId']
-        character = data['character']
-
-        respData = {}
-        courseList = tableCourseTeacher[userId]
-        courseDetail = []
-        courseCount = len(courseList)
-        for i in range(0, courseCount):
-            courseId = courseList[i]
-            topicIdList = tableTopicCourse[courseId]
-            topicList = map(lambda x: tableTopicCourse[x]['topicTitle'], topicIdList)
-            cd = {
-                "title": tableCourse[courseId]['courseTitle'],
-                "count": len(tableTopicCourse[courseId]),
-                "topicList": topicList
-            }
-            courseDetail.append(cd)
-
-        respData = {
-            "count": courseCount,
-            "courses": courseDetail
-        }
-
-        return respData
+        return data
+        # userId = data['userId']
+        # character = data['character']
+        #
+        # respData = {}
+        # courseList = tableCourseTeacher[userId]
+        # courseDetail = []
+        # courseCount = len(courseList)
+        # for i in range(0, courseCount):
+        #     courseId = courseList[i]
+        #     topicIdList = tableTopicCourse[courseId]
+        #     topicList = map(lambda x: tableTopicCourse[x]['topicTitle'], topicIdList)
+        #     cd = {
+        #         "title": tableCourse[courseId]['courseTitle'],
+        #         "count": len(tableTopicCourse[courseId]),
+        #         "topicList": topicList
+        #     }
+        #     courseDetail.append(cd)
+        #
+        # respData = {
+        #     "count": courseCount,
+        #     "courses": courseDetail
+        # }
+        #
+        # return respData
