903581caa94abc6dae43f759a924e4f3c95a22e1
王天晓 <wangtianxiao@wangtianxiaos-MacBook-Pro.local>
2018-04-25 20:38:36 +0800

    add studentcourselist api and html check

--- api/app.py
+++ api/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, Response
 from flask.ext.restful import Api, Resource
 from flask_cors import CORS
-from course import CreateCourse, TeacherCourseList
+from course import CreateCourse, TeacherCourseList, StudentCourseList
 
 application = app = Flask(__name__)
 # app.response_class = MyResponse
@@ -10,6 +10,7 @@ CORS(app, origins='*', allow_headers='*')
 
 api.add_resource(CreateCourse, '/createCourse', endpoint="createCourse")
 api.add_resource(TeacherCourseList, '/teacherCourseList')
+api.add_resource(StudentCourseList, '/studentCourseList')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- api/course.py
+++ api/course.py
@@ -5,9 +5,14 @@ from flask.ext.restful import Resource
 from uuid import uuid4
 
 tableCourse = {}
+# courseId: {courseTitle, courseDescription, teacherId}
 tableCourseStudent = {}
+#
 tableCourseTeacher = {}
+# userId: List<courseId>
 tableTopicCourse = {}
+# courseId: List<topicId>, topicId: {courseId, topicTitle}
+
 
 class CreateCourse(Resource):
     # 新建课程
@@ -20,11 +25,16 @@ class CreateCourse(Resource):
         topics = data['topics']['details']
 
         courseId = str(uuid4())
-        tableCourseTeacher[userId] = []
-        tableCourseTeacher[userId].append(courseId)
+
+        if userId in tableCourseTeacher :
+            tableCourseTeacher[userId].append(courseId)
+        else :
+            tableCourseTeacher[userId] = [courseId, ]
+
         tableCourse[courseId] = {
             'courseTitle': courseTitle,
-            'courseDescription': courseDescription
+            'courseDescription': courseDescription,
+            'teacherId': userId
         }
         tableTopicCourse[courseId] = []
         for i in range(0, topicCount):
@@ -43,29 +53,57 @@ class CreateCourse(Resource):
 class TeacherCourseList(Resource):
     # Fetch 课程列表
     def get(self):
-        data = request.get_json() or request.form
-        return data
-        # userId = data['userId']
-        # character = data['character']
-        #
-        # respData = {}
-        # courseList = tableCourseTeacher[userId]
-        # courseDetail = []
-        # courseCount = len(courseList)
-        # for i in range(0, courseCount):
-        #     courseId = courseList[i]
-        #     topicIdList = tableTopicCourse[courseId]
-        #     topicList = map(lambda x: tableTopicCourse[x]['topicTitle'], topicIdList)
-        #     cd = {
-        #         "title": tableCourse[courseId]['courseTitle'],
-        #         "count": len(tableTopicCourse[courseId]),
-        #         "topicList": topicList
-        #     }
-        #     courseDetail.append(cd)
-        #
-        # respData = {
-        #     "count": courseCount,
-        #     "courses": courseDetail
-        # }
-        #
-        # return respData
+        # data = request.args.get("userId")
+        userId = request.args.get("userId")
+        character = request.args.get("character")
+
+        respData = {}
+        courseList = tableCourseTeacher[userId]
+        courseDetail = []
+        courseCount = len(courseList)
+        for i in range(0, courseCount):
+            courseId = courseList[i]
+            topicIdList = tableTopicCourse[courseId]
+            topicList = list(map(lambda x: tableTopicCourse[x]['topicTitle'], topicIdList))
+            cd = {
+                "title": tableCourse[courseId]['courseTitle'],
+                "count": len(tableTopicCourse[courseId]),
+                "topicList": topicList
+            }
+            courseDetail.append(cd)
+
+        respData = {
+            "count": courseCount,
+            "courses": courseDetail
+        }
+
+        return respData
+
+class StudentCourseList(Resource):
+    # Fetch Student Course List
+    def get(self):
+        userId = request.args.get("userId")
+        character = request.args.get("character")
+
+        respData = {}
+        courseList = tableCourseTeacher[userId]
+        courseDetail = []
+        courseCount = len(courseList)
+        for i in range(0, courseCount) :
+            courseId = courseList[i]
+            courseTitle = tableCourse[courseId]['courseTitle']
+            courseDescription = tableCourse[courseId]['courseDescription']
+            teacherId = tableCourse[courseId]['teacherId']
+            cd = {
+                "title": courseTitle,
+                "description": courseDescription,
+                "id": courseId,
+                "teacher": teacherId
+            }
+            courseDetail.append(cd)
+
+        respData = {
+            "count": courseCount,
+            "courses": courseDetail
+        }
+        return respData
