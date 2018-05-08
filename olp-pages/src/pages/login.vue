0637bca1de90e6ec72bf9d636436c04107414b3b
Gevolll <tianxiao.wang@qq.com>
2018-05-08 20:39:46 +0800

    add login.vue

---
+++ olp-pages/src/pages/login.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="login">
+    <div class="login-form">
+      <div class="label">
+        <p>登录学习，发现更大的世界</p>
+      </div>
+      <div class="label">
+        <Input type="text" name="username" v-model="username" placeholder="用户名" size="large" clearable></Input>
+      </div>
+      <div class="label">
+        <Input type="password" name="password" v-model="password" placeholder="密码" size="large" clearable></Input>
+      </div>
+      <div class="label">
+        <Button type="primary" name="login-button" long @click="login()">登录</Button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    data () {
+      return {
+        username: '',
+        password: ''
+      }
+    },
+    methods: {
+      login () {
+        // alert(this.username+this.password);
+        var username = this.username;
+        var password = this.password;
+
+        if (username === '') {
+          alert("请输入学/工号！");
+          return;
+        }
+        if (password === '') {
+          alert("请输入密码！");
+          return;
+        }
+        var usernameRe = /^[0-9]+.?[0-9]*/;
+        if(!usernameRe.test(username)) {
+          alert("请输入正确的学/工号！");
+          return;
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+.label {
+  width: 300px;
+  position: relative;
+  left: 50%;
+  margin-left: -150px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+.label>p {
+  color: #2d8cf0;
+  font-size: 1.25rem;
+  font-weight: bold;
+  padding-bottom: 10px;
+}
+.login-form {
+  background-color: #ffffff;
+  width: 400px;
+  border-radius: 8px;
+  border: 1px solid #dddee1;
+  position: relative;
+  left: 50%;
+  top: 10%;
+  margin-left: -200px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+.login {
+  height: 100%;
+  background-color: #b8e5f8;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url(../assets/bkg.png);
+}
+input:focus {
+  outline: 0;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+}
+</style>
